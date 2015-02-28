9b68ff045b07173ce92b304048311925a1e3a1eb
Satarupa Das <satarupa@inkoniq.com>
2015-02-28 11:47:34 +0530

    js files

---
+++ public/ngApp/ngController/AddBrandController.js
@@ -0,0 +1,46 @@
+/*
+    Controller to 
+        Add brand
+*/
+OnliofliApp.controller('AddBrandController', function ($scope, $location, $http) {  
+    $scope.AddBrandData = {
+        "brand_name" : "",
+        "brand_logo" : "",
+        "brand_address" : "",
+        "brand_contact_name" : "",
+        "brand_contact_details" : "",
+    };
+    $scope.addbrand = function(){
+        // checking validations
+        // var b = $("form.addProperty").valid();
+        // if(!(b)) {
+        //     return false;
+        // }
+        // freeze further action
+        console.log('entered');
+        jQuery('#overlay').show();
+        $http({
+            method  : 'POST',
+            url     : '/brand',
+            dataType : 'JSON',
+            data    : $scope.AddBrandData
+        }).success(function (result){
+            jQuery('#overlay').hide();
+            if(result.status_code == 200){
+                bootbox.alert('Congratulations! Your brand has been added successfully.');
+
+            }
+            else if(result.status_code == 401){
+                window.location.href="/";
+            }
+            else if(result.status_code == 400)
+            {
+                bootbox.alert(result.data.Reason);
+            }
+            else{
+                window.location.href = "error";
+            }
+        });
+    };
+
+});
